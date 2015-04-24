67abb52c2b635f166ac6e2be79d0cb75c5d50e9c
Michael Nisi <michael.nisi@gmail.com>
2015-04-24 15:07:23 +0200

    Comply with standard

--- example/read.js
+++ example/read.js
@@ -1,18 +1,17 @@
-
 // read - read one octet at a time
 
 var stread = require('../')
 
 var reader = stread('You know what it is to be born alone, Baby tortoise!')
-  , writer = process.stdout
+var writer = process.stdout
 
 function write () {
+  var chunk
   var ok = true
-    , chunk = null
-  while (null !== (chunk = reader.read(1)) && ok) {
+  while (ok && (chunk = reader.read(1)) !== null) {
     ok = writer.write(chunk)
   }
-  if (!ok) writer.once('drain', write);
+  if (!ok) writer.once('drain', write)
 }
 
 write()
--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-
 // stread - stream string
 
 module.exports = ReadableString
 
 var util = require('util')
-  , Readable = require('stream').Readable
+var Readable = require('readable-stream').Readable
 
 util.inherits(ReadableString, Readable)
 function ReadableString (str) {
   if (!(this instanceof ReadableString)) return new ReadableString(str)
   Readable.call(this)
-  this.buf = new Buffer(str)
+  this._buf = new Buffer(str)
 }
 
 ReadableString.prototype._read = function (size) {
   var ok = true
-    , chunk = null
-    , end = 0
+  var chunk
+  var end = 0
   do {
-    var buf = this.buf
+    var buf = this._buf
     if (buf === null || buf.length === 0) {
       this.push(null)
       break
@@ -26,6 +25,6 @@ ReadableString.prototype._read = function (size) {
     end = size && size < buf.length ? size : buf.length
     chunk = buf.slice(0, end)
     ok = this.push(chunk)
-    this.buf = buf.slice(end, buf.length - end)
+    this._buf = buf.slice(end, buf.length - end)
   } while (ok && end > 0)
 }
