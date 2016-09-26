c4998061e48720728c771b6a4c9667082518f8c4
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2016-09-26 15:19:50 -0700

    Fixes chunked reading

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 // stread - stream string
 
-module.exports = stread
+module.exports = ReadableString
 
-const PassThrough = require('readable-stream').PassThrough
+var util = require('util')
+var Readable = require('readable-stream').Readable
 
-function stread (str, encoding) {
-  encoding = encoding || 'utf8'
-  const s = new PassThrough({ encoding: encoding })
-  s.end(str)
-  return s
+function ReadableString (str) {
+  if (!(this instanceof ReadableString)) return new ReadableString(str)
+  Readable.call(this)
+  this._buf = new Buffer(str)
+}
+util.inherits(ReadableString, Readable)
+
+ReadableString.prototype._read = function (size) {
+  var ok = true
+  var chunk
+  var end = 0
+  do {
+    var buf = this._buf
+    if (buf === null || buf.length === 0) {
+      this.push(null)
+      this._buf = null
+      break
+    }
+    end = size && size < buf.length ? size : buf.length
+    chunk = buf.slice(0, end)
+    ok = this.push(chunk)
+    this._buf = buf.slice(end, buf.length)
+  } while (ok && end > 0)
 }
