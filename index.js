286f6a002e5577d30cc143799dbdee4c8d217738
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2016-09-27 10:56:48 -0700

    Uses the same buffer for reading to avoid memory churn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function ReadableString (str) {
   if (!(this instanceof ReadableString)) return new ReadableString(str)
   Readable.call(this)
   this._buf = new Buffer(str)
+  this._pos = 0
 }
 util.inherits(ReadableString, Readable)
 
@@ -18,14 +19,15 @@ ReadableString.prototype._read = function (size) {
   var end = 0
   do {
     var buf = this._buf
-    if (buf === null || buf.length === 0) {
+    if (buf === null || this._pos >= buf.length) {
       this.push(null)
       this._buf = null
+      this._pos = 0
       break
     }
-    end = size && size < buf.length ? size : buf.length
-    chunk = buf.slice(0, end)
+    end = size && this._pos + size < buf.length ? this._pos + size : buf.length
+    chunk = buf.slice(this._pos, end)
     ok = this.push(chunk)
-    this._buf = buf.slice(end, buf.length)
+    this._pos += end
   } while (ok && end > 0)
 }
